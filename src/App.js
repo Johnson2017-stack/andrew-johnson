8ebdadaba82d456a48d16bd97c3c3046575f37b8
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-07-23 18:42:15 -0700

    rerouter pages to pages folder, fonfigured css to styles.css, made nav my only component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 
 import './App.css';
-import Home from './components/Home';
+import Nav from './components/Nav';
+import Home from './Pages/Home';
+import About from "./Pages/About";
+import Porfolio from "./Pages/Portfolio";
+import Contact from "./Pages/Contact";
+
+
 
 function App() {
+  let component
+  switch (window.location.pathname){
+    case "/":
+      component = <Home />
+      break
+    case "/about":
+      component = <About/>
+      break
+    case "/portfolio":
+      component = <Porfolio />
+      break
+    case "/contact":
+      component = <Contact />
+      break
+  }
   return (
     <div className="App">
       <>
-      <Home />
+      <Nav />
       </>
    
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,30 @@
 import React from 'react'
 
-
-
 const Nav = () => {
   return (
     <>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Andrew.</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/Home.jsx">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./About"> About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Portfolio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  <nav className="nav">
+    <a href="/" className='site-title'>Johnson .</a>
+    <ul>
+      <li>
+        <a href="/home">Home</a>
+      </li>
+   
+    <li>
+        <a href="/about">About</a>
+    </li>
+    
+      <li>
+        <a href="/portfolio">Portfolio</a>
+      </li>
+    
+    <li>
+      <a href='/contact'>Contact</a>
+    </li>
+    </ul>
+
+  </nav>
+
 </>
 
 
