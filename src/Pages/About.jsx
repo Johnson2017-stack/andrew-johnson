f80e6235b0ac834971d1039911ea4c7f476a43d0
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-07-23 23:53:33 -0700

    finished up styling and putting together my about page and also removed any unnecessary photos

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,18 +5,34 @@ const About = () => {
   return (
     <>
       <h1>About</h1>
+      <hr/>
       <div>
         <p id="biography">
-          Hello. I am striving to work with a team to build a full stack
-          application. I have expertise in HTML, CSS, Node, EJS, and Javascript.
-          I just built an app referencing a nasa api using EJS and Javascript,
-          also my weather app in my GitHub is built with javascript using react
-          and deployed with Netlify.
+         <h4> Hello, </h4>
+         I am striving to work with a team to build a full stack
+          application. I am finishing up a full stack MERN software development course with SkillSpire, I Graduate at the end of August. I have several Works showcased in my portfolio page on this app go check them out to see if my skill set is what you are looking for. I look forward from hearing from you soon and working with your team, Talk with you soon.
           </p>
           <hr />
           <div id="skills">
-            Skills: EJS HTML CSS Flex-box Boot strap JAVASCRIPT React Node Rest
-            Api’s Software development UX/UI Microsoft office Excell
+            <h3> Skills: </h3>
+            <hr/>
+            <p className="skills">
+                <p>EJS</p>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>Flex-box</p>
+                <p>Boot strap</p>
+                <p>JAVASCRIPT</p>
+                <p>React</p>
+                <p>Node</p>
+                <p>Rest Api’s</p>
+                <p>Software development</p>
+                <p>Microsoft office</p>
+                <p>Excell</p>
+                <p>MongoDB</p>
+                <p>MySQL</p>
+                <p>Express</p>
+                </p>
           </div>
        
       </div>
